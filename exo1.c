6e5dddce58332b3488c0c938ff2fefc37617a69b
Benoit <Benoit.poulet@edu.ece.fr>
2022-11-25 10:09:06 +0100

    main

---
+++ exo1.c
@@ -0,0 +1,5 @@
+//
+// Created by Benoi on 25/11/2022.
+//
+
+#include "exo1.h"
---
+++ exo1.h
@@ -0,0 +1,8 @@
+//
+// Created by Benoi on 25/11/2022.
+//
+
+#ifndef TP6BIS_EXO1_H
+#define TP6BIS_EXO1_H
+
+#endif //TP6BIS_EXO1_H
